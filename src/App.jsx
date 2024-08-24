499e9797d71b9a17a98a19c83bcc95da19de20c4
rishowrav <rishowrav@gmail.com>
2024-08-24 22:02:11 +0600

    service section design has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services/Services";
 
 export default function App() {
   return (
     <>
       <main className="overflow-x-hidden bg-white text-dark">
         <Hero />
+        <Services />
       </main>
     </>
   );
