5a13e38cf06c226c606460d5a5520d74ac2d4c08
rishowrav <rishowrav@gmail.com>
2024-08-25 11:08:30 +0600

    footer section design has been completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Banner from "./components/Banner/Banner";
 import Banner2 from "./components/Banner/Banner2";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
 import Subscribe from "./components/Subscribe/Subscribe";
@@ -13,6 +14,7 @@ export default function App() {
         <Banner />
         <Subscribe />
         <Banner2 />
+        <Footer />
       </main>
     </>
   );
